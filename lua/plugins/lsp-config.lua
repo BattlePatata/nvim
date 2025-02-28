c2c71abd48a93ab1c6ed1d7af5bd90414c3d0138
BattlePatata <ovez.choliev@gmail.com>
2025-02-28 12:40:38 +0500

    Nvim config
    My Neovim configuration file

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,56 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.stimulus_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.intelephense.setup({
+				capabilities = capabilities,
+                root_dir = lspconfig.util.root_pattern(".git", "composer.json"),
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+						}
+					}
+				}
+			})
+			require("telescope").load_extension("ui-select")
+		end
+	},
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,46 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+        parser_config.blade = {
+            install_info = {
+                url = "~/blade_parser/tree-sitter-blade",
+                files = { "src/parser.c" },
+                branch = "main",
+            },
+            filetype = "blade"
+        }
+
+        vim.filetype.add({
+            pattern = {
+                [".*%.blade%.php"] = "blade",
+            },
+        })
+
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"html",
+				"php_only",
+				"php",
+				"bash",
+				"css",
+				"javascript",
+				"lua",
+				"c",
+				"dockerfile",
+				"xml",
+				"vim",
+				"xml",
+				"c_sharp",
+				"cmake",
+				"cpp",
+			},
+			sync_install = true,
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
